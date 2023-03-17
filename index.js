107c54464fff19547b3cc3803a3682a163a30f31
Kennedy <kiplangatkennedy019@gmail.com>
2023-03-17 17:47:51 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
+      // ---scuberGreetingForFeet-->
+      
+function scuberGreetingForFeet(myNumber){
   // Write your code here!
+  if(myNumber == 199){
+    return "This one is on me!"
+  }
+  else if (myNumber> 2000 && myNumber < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else if (myNumber > 2500){
+    return "No can do."
+  }
 }
+      // --ternaryCheckCity-->
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(myCity){
   // Write your code here!
+if (myCity == "NYC"){
+  return "Ok, sounds good."
+}
+else{
+  return "No go.";
+}
+}
+        // --switchOnCharmFromTip--
+
+function switchOnCharmFromTip(myTip){
+  // Write your code here!
+   
+ switch(myTip){
+   case 'generous':
+     return "Thank you so much."
+     break;
+   case 'not as generous':
+   return "Thank you."
+   break;
+   default:
+     return "Bye."
+ 
+ }
 }
